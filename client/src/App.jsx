ff57e23df168b5fab04d45be8c53644bc9361820
xaless <hsuwang75@gmail.com>
2024-06-02 15:27:18 +0200

    test ci

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,6 @@ function App() {
 
   return (
     <>
-        // This is a test comment to trigger CI
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />

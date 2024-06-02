a63047ec08931c094497ebb102bd3f80edb28090
xaless <hsuwang75@gmail.com>
2024-06-02 15:42:07 +0200

    config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+/* eslint-env node */
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Challenge-React-ApiPlatforme/' : '/'
-});
+  define: {
+    'process.env': process.env
+  }
+})

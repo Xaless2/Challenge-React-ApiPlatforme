445183575647b8faa8d4250fc82d4a7608d107c6
Manu1Mackenzii <emmanuelit29@gmail.com>
2024-06-27 13:49:44 +0200

    merge

--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -1,134 +1,33 @@
-import React, { createContext, useState, useCallback, useEffect } from 'react';
-import { postRequest, baseUrl, updateRequest, authUrl, getRequest } from '../utils/service';
-import { jwtDecode } from "jwt-decode";
+import React, { createContext, useState, useEffect } from 'react';
 
 export const AuthContext = createContext();
 
-export const AuthContextProvider = ({ children }) => {
+const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
-    const [error, setError] = useState(null);
-    const [token, setToken] = useState(null);
-    const [userRole, setUserRole] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
-
 
     useEffect(() => {
-        if (token) {
-            const decodedToken = jwtDecode(token); 
-            setUserRole(decodedToken.roles); 
-        }
-    }, [token]);
-
-
-    const registerUser = useCallback(async (data) => {
-        setIsLoading(true);
-        try {
-            const response = await postRequest(
-                `${authUrl}/register`,
-                data
-            );
-            if (response && (response.user || response.token)) {
-                setUser(response.user);
-                setToken(response.token);
-                localStorage.setItem('token', response.token);
-                localStorage.setItem('currentUser', JSON.stringify(response.user));
-            }
-        } catch (error) {
-            setError(error?.message || error);
-        }
-        setIsLoading(false);
-    }, []);
-
-    const loginUser = useCallback(async (data) => {
-        setIsLoading(true);
-        try {
-            const response = await postRequest(`${authUrl}/login`, data);
-            if (response && response.token) {
-                setToken(response.token);
-                localStorage.setItem('token', response.token);
-                const decodedToken = jwtDecode(response.token);
-                setUser(decodedToken.user); 
-                return true;  
-            }
-        } catch (error) {
-            setError(error?.message || error);
-        }
-        setIsLoading(false); 
-        return false; 
-    }, []);
-
-    const updateUser = useCallback(async (data) => {
-        const token = localStorage.getItem('token');
-        if (!token) {
-            return;
-        }
-    
-        try {
-            const response = await updateRequest(
-                `${baseUrl}/users/${user.id}`,
-                data,{
-                    "Authorization": `Bearer ${token}`,
-                }
-            );
-            setUser(response);
-            localStorage.setItem('currentUser', JSON.stringify(response));
-        } catch (error) {
-            setError(error?.message || error);
-        }
-    
-    }, [user]);
-
-    const getUser = useCallback(async () => {
-        const token = localStorage.getItem('token');
-        if (!token) {
-            return;
-        }
-    
-        try {
-            const response = await getRequest(
-                `${baseUrl}/me`,
-                {
-                    "Authorization": `Bearer ${token}`,
-                }
-            );
-            setUser(response);
-            return response; 
-        } catch (error) {
-            setError(error?.message || error);
+        // Check if the user is authenticated
+        const loggedInUser = localStorage.getItem('user');
+        if (loggedInUser) {
+            setUser(JSON.parse(loggedInUser));
         }
     }, []);
-      
-    
-   useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        setToken(token);
-    }
-    setIsLoading(false);
-}, []);
+
+    const login = (userData) => {
+        setUser(userData);
+        localStorage.setItem('user', JSON.stringify(userData));
+    };
 
     const logout = () => {
         setUser(null);
-        setToken(null);
-        localStorage.removeItem('token');
-        localStorage.removeItem('currentUser');
+        localStorage.removeItem('user');
     };
 
     return (
-        <AuthContext.Provider value={{ 
-            registerUser, 
-            loginUser,
-            token,
-            logout,
-            error,
-            isLoading,
-            userRole,
-            setUserRole,
-            updateUser,
-            getUser,
-            user,
-        }}>
+        <AuthContext.Provider value={{ user, login, logout }}>
             {children}
         </AuthContext.Provider>
     );
 };
+
+export default AuthProvider;
